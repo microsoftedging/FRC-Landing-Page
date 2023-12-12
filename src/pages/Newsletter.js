1a43b66943c0a2dc7e6ee7718339bd94b218fa18
microsoftedging <pnla100@student.hbuhsd.edu>
2023-12-11 16:47:10 -0800

    initial

--- src/pages/Newsletter.js
+++ src/pages/Newsletter.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export default function Newsletter() {
-  return (
-    <div>Newsletter</div>
-  )
+export default function Newsletter({ title, content, header }) {
+    return (
+      <div className="newsletter">
+        <h2>{title}</h2>
+        {header && (
+            <div className="header">
+                {header.map((header, index))}
+        )}
+      </div>  
+    )
 }
+
+export default Newsletter
